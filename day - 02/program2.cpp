319201f4d5ea7845b06c9b1f8f4f2f9c6bfc22d6
ridhii23 <ridhirana07@gmail.com>
2026-06-06 13:36:14 +0530

    day 2 uploaded

---
+++ day - 02/program2.cpp
@@ -0,0 +1,15 @@
+//code to reverse a number
+#include <stdio.h>
+int main()
+{
+    int n, rev = 0;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+    while (n > 0)
+    {
+        rev = rev * 10 + n % 10;  
+        n /= 10;                   // removes the last digit from the number
+    }
+    printf("Reversed number: %d\n", rev);
+    return 0;
+}
