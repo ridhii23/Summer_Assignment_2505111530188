438515cffa6795cd0421e50214a3a2fd70f662f9
ridhii23 <ridhirana07@gmail.com>
2026-06-06 12:30:00 +0530

    your message

---
+++ day - 01/program2.cpp
@@ -0,0 +1,13 @@
+//code to print multiplication table of a given number
+#include<stdio.h>
+int main()
+{
+    int i,n;
+    printf("provide a number n");
+    scanf("%d",&n);
+    for(i=1;i<=10;i++)
+    {
+        printf("%d x %d = %d\n",n,i,n*i);
+       }
+       return 0;
+}
