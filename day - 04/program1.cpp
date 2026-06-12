9a5c5312211a32abf636dd4ed40a6cb0d42a0c09
ridhii23 <ridhirana07@gmail.com>
2026-06-12 22:48:44 +0530

    day 04 uploaded

---
+++ day - 04/program1.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    int n, a = 0, b = 1, newterm;
+
+    printf("enter the last term of series: ");
+    scanf("%d", &n);
+
+    printf("%d %d ", a, b);
+
+    newterm = a + b;
+
+ 
+    while (newterm <= n) {
+        printf("%d ", newterm);
+        a = b;
+        b = newterm;
+        newterm = a + b;
+    }
+ 
+    return 0;
+}
