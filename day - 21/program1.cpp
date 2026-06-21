8ee735bc676d15800de4216f90e5c2eb951e75df
ridhii23 <ridhirana07@gmail.com>
2026-06-21 14:06:36 +0530

    Create program1.cpp

---
+++ day - 21/program1.cpp
@@ -0,0 +1,20 @@
+//find string length without strlen()
+#include <stdio.h>
+
+int main()
+{
+    char string[100];
+    int length = 0;
+
+    printf("Enter a string: ");
+    scanf("%s", string);
+
+    while(string[length] != '\0')
+    {
+        length++;
+    }
+
+    printf("Length of string = %d", length);
+
+    return 0;
+}
