f53db71723a8fa5bb07f16251b07277a7ad01a81
ridhii23 <ridhirana07@gmail.com>
2026-06-24 21:53:12 +0530

    Add character frequency calculation program
    This program calculates and displays the frequency of each character in a given string input by the user.

---
+++ day - 22/program3.cpp
@@ -0,0 +1,26 @@
+//code to find character frequency.
+#include <stdio.h>
+
+int main() {
+    char s[100];
+    int f[256] = {0},i;
+
+    printf("Enter a string: ");
+    scanf("%[^\n]", s);
+
+    for(i = 0;s[i] != '\0'; i++)
+    {
+        f[(unsigned char)s[i]]++;
+    }
+
+    printf("\nCharacter Frequencies:\n");
+    for(i = 0; i < 256; i++)
+    {
+        if(f[i] != 0) 
+        {
+            printf("%c = %d\n", i, f[i]);
+        }
+    }
+
+    return 0;
+}
