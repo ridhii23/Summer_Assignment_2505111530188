a0e0e0ba620dffdab85629e95ca599626f705681
ridhii23 <ridhirana07@gmail.com>
2026-06-24 21:56:42 +0530

    Create program1.cpp

---
+++ day - 23/program1.cpp
@@ -0,0 +1,26 @@
+//code to find first non-repeating character.
+#include <stdio.h>
+int main() {
+    char s[100];
+    int f[256] = {0};
+    int i; 
+  printf("Enter a string: ");
+  scanf("%[^\n]", s);
+
+    for(i = 0; s[i] != '\0'; i++)
+      {
+        f[(unsigned char)s[i]]++;
+    }
+
+        for(i = 0; s[i] != '\0'; i++)
+          {
+        if(f[(unsigned char)s[i]] == 1) {
+            printf("First non-repeating character: %c\n", s[i]);
+            return 0;
+        }
+    }
+
+    printf("Nothing found.\n");
+
+    return 0;
+}
