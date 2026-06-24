70ead29ac175618951c54cc2385ea085d058feae
ridhii23 <ridhirana07@gmail.com>
2026-06-24 21:51:46 +0530

    Create program2.cpp

---
+++ day - 22/program2.cpp
@@ -0,0 +1,23 @@
+//count words in a sentence.
+#include <stdio.h>
+
+int main() 
+{
+    char s[100];
+    int i, words = 1;
+
+    printf("Enter a string: ");
+    scanf("%[^\n]", s);
+
+    for(i = 0; s[i] != '\0'; i++) 
+    {
+        if(s[i] == ' ')
+        {
+            words++;
+        }
+    }
+
+    printf(" The number of words = %d", words);
+
+    return 0;
+}
